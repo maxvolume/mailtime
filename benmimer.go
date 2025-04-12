b3c3618058a956eb977b2793d8c18cef02da3852
maxvolume321 <ben@schonbeck.io>
2025-04-11 21:20:12 -0400

    Testing basic mbox file access...

--- benmimer.go
+++ benmimer.go
@@ -1,7 +1,28 @@
 package main
 
-import ("fmt")
+import (
+    "fmt"
+    "os"
+    "bufio"
+)
 
 func main() {
     fmt.Println("Hi there from benmimer...")
+    file, err := os.Open("/home/ben/projects/gostuff/Takeout/Mail/benmail.mbox")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    data := make([]byte, 500)
+    n, err := reader.Read(data)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Printf("Read %d bytes from mail data...\n", n)
+    fmt.Println(string(data))
 }
