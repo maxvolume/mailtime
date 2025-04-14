1f5b192978626013f45f9f9eb0ebe043bc49dcba
maxvolume321 <ben@schonbeck.io>
2025-04-14 09:17:57 -0400

    Basic mbox parse setup.

--- benmimer.go
+++ benmimer.go
@@ -4,6 +4,9 @@ import (
     "fmt"
     "os"
     "bufio"
+    "bytes"
+    "net/mail"
+    "mime"
 )
 
 func main() {
@@ -16,12 +19,32 @@ func main() {
     defer file.Close()
 
     reader := bufio.NewReader(file)
-    for range 4 {
+    decoder := new(mime.WordDecoder)
+    msgbuffer := make([]byte, 200000)
+    prefix := []byte("From ")
+    count := 0
+    for count < 2 {
         data, err := reader.ReadBytes('\n')
         if err != nil {
             fmt.Println(err)
             return
         }
-        fmt.Println(string(data))
+        if bytes.HasPrefix(data, prefix) {
+            if count != 0 {
+                msg, err := mail.ReadMessage(bytes.NewReader(msgbuffer))
+                if err != nil {
+                    fmt.Println("Error parsing data msg buffer", err)
+                    break
+                }
+                from, _ := decoder.DecodeHeader(msg.Header.Get("From"))
+                fmt.Printf("From: %s\tTo: %s\tSubject: %s\n", from, msg.Header.Get("To"), msg.Header.Get("Subject"))
+            }
+            msgbuffer = msgbuffer[:0]
+            fmt.Println("Capacity of msgbuffer: ", cap(msgbuffer))
+            msgbuffer = append(msgbuffer, data...)
+            count++
+        } else {
+            msgbuffer = append(msgbuffer, data...)
+        }
     }
 }
