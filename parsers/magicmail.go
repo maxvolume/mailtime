7e234b598efd657125b5b7fd163c8614dddf78f6
maxvolume <ben@schonbeck.io>
2025-04-27 14:51:54 -0400

    Basic text extraction (#2)
    * Starting on body parsing
    
    * Initial text mime/multipart parsing
    
    * Continue to next part if we have error on IO read
    
    * Completed basic text multipart extraction

--- parsers/magicmail.go
+++ parsers/magicmail.go
@@ -25,13 +25,16 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"mime"
+	"mime/multipart"
 	"net/mail"
 	"os"
+	"strings"
 )
 
-func ParseMbox(filename string, peekNumber int) {
-	fmt.Println("Hi there from benmimer...")
+func ParseMbox(filename string, peekNumber int, peekText bool) {
+	fmt.Printf("%s\n\n", "Hi there from benmimer...")
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err)
@@ -44,7 +47,7 @@ func ParseMbox(filename string, peekNumber int) {
 	msgbuffer := make([]byte, 200000)
 	prefix := []byte("From ")
 	count := 0
-	for count < peekNumber {
+	for count <= peekNumber {
 		data, err := reader.ReadBytes('\n')
 		if err != nil {
 			fmt.Println(err)
@@ -58,14 +61,60 @@ func ParseMbox(filename string, peekNumber int) {
 					break
 				}
 				from, _ := decoder.DecodeHeader(msg.Header.Get("From"))
-				fmt.Printf("From: %s\tTo: %s\tSubject: %s\n", from, msg.Header.Get("To"), msg.Header.Get("Subject"))
-			}
+                mailTo := msg.Header.Get("To")
+                msgSubject := msg.Header.Get("Subject")
+                contentType := msg.Header.Get("Content-Type")
+                fmt.Printf("From: %s\tTo: %s\tSubject: %s\n", from, mailTo, msgSubject)
+                fmt.Printf("Content-Type: %s\n\n", contentType)
+                mediaType, params, err := mime.ParseMediaType(contentType)
+                if err != nil {
+                    fmt.Printf("Error parsing media type %s: %s\n", mediaType, err)
+                }
+                if strings.HasPrefix(mediaType, "multipart/") {
+                    parseMultiPart(msg.Body, params["boundary"], peekText)
+                }
+                fmt.Printf("\n%s\n\n", ">>>>>>>>>>>>>>>>>>>>>")
+            }
 			msgbuffer = msgbuffer[:0]
-			fmt.Println("Capacity of msgbuffer: ", cap(msgbuffer))
+            // NOTE: maybe add this behind a verbose flag fmt.Println("Capacity of msgbuffer: ", cap(msgbuffer))
 			msgbuffer = append(msgbuffer, data...)
 			count++
 		} else {
 			msgbuffer = append(msgbuffer, data...)
 		}
 	}
+}
+
+func parseMultiPart (mimedata io.Reader, boundary string, peekText bool) {
+    reader := multipart.NewReader(mimedata, boundary)
+    if reader == nil {
+        return
+    }
+    for {
+        part, err := reader.NextPart()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            fmt.Println("Error parsing multipart section: ", err)
+            break
+        }
+        mediaType, params, err := mime.ParseMediaType(part.Header.Get("Content-Type"))
+        if err != nil {
+            fmt.Printf("Error parsing media type %s: %s\n", mediaType, err)
+            continue
+        }
+        fmt.Printf("Part Media Type: %s\n", mediaType)
+        if strings.HasPrefix(mediaType, "multipart/") {
+            parseMultiPart(part, params["boundary"], peekText)
+        }
+        if strings.HasPrefix(mediaType, "text/plain") && peekText {
+            content, err := io.ReadAll(part)
+            if err != nil {
+                fmt.Println("Error reading text part: ", err)
+				continue
+            }
+            fmt.Printf("\n%s\n\n", string(content))
+        }
+    }
 }
